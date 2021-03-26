fa6ce1304862922424bde8ca20ce5ce9b417979e
Almazhan Kapan <aa5456@nyu.edu>
2021-03-26 19:27:59 +0800

    connect back-end to front-end draft 2

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Interface for HRL search page (draft 1)
 
 Update 1/3:
 created main.js, results.js, updated results.html, index.html and stylesheets
+
+Update 26/3:
+connect to draft 2 front-end, allow search b
--- results.js
+++ results.js
@@ -1,44 +1,95 @@
 var jsonOut=localStorage.getItem("output");
 var jsonArray=JSON.parse(jsonOut);
-console.log(jsonArray[0].ID);
 
+//get from local storage
+var jsonCheckin=localStorage.getItem("check-in");
+var jsonCheckout=localStorage.getItem("check-out");
+console.log(jsonCheckout);
+
+//parse the json string array to an actual array
+var arrayCheckin=JSON.parse(jsonCheckin);
+console.log(arrayCheckin);
+
+var arrayCheckout=JSON.parse(jsonCheckout);
+console.log(arrayCheckout);
+
+
+function outContains(array, key){
+  return (array.indexOf(key) > -1);
+}
 
 
 function clear(){
-    document.getElementById("app").textContent= "";
-  }
-  
+
+  document.getElementById("app").textContent= "";
+}
+
   function table_populate(){
-    
-      
+    var table=document.getElementById('table_results');
+
+    var outnumber=arrayCheckout.length+1; 
+    var count=0;
+    var table_row = document.createElement('tr');
+    n_arr=[];
+    n_arr[0]=document.createElement('td');
+    n_arr[0].innerHTML="ID";
+    table_row.appendChild(n_arr[0]);
+    for (j=0;j<outnumber; j++){
+      n_arr[j+1]=document.createElement('td');
+      n_arr[j+1].innerHTML=arrayCheckout[j];
+      table_row.appendChild(n_arr[j]);
+    }
+    table.appendChild(table_row);
       
      console.log("entered");
-      var table=document.getElementById('table_results');
       if (table != null) console.log("something living");
-      var i,j;
+      var i,j,r;
       var categ=8; //to be changed based on user
-      for (i=0;i<jsonArray.length; i++){
+      for (r=0;r<jsonArray.length; r++){
+      for (i=0;i<jsonArray[r].length; i++){
+        var id, year, dir, address, nation, name;
         var table_row = document.createElement('tr');
-        var data_row=jsonArray[i];
-        var id=data_row.ID;
-        var year=data_row.YR;
-        var dir=data_row.DIR;
-        var address=data_row.ADDY;
-        var nation=data_row.NATIONALITY;
-        var name=data_row.NAME;
-        var data=[id, year, dir, nation, name,"unavailable","unavailable",address ];
+        var data=[];
+        var data_row=jsonArray[r][i];
+        // if (outContains(arrayCheckout, "ID")){
+        id=data_row.ID;
+        data.push(id);
+      // } 
+        if (outContains(arrayCheckout, "YR")){
+        year=data_row.YR;
+        data.push(year);
+        }
+          if (outContains(arrayCheckout, "DIR")){
+        dir=data_row.DIR;
+        data.push(dir);
+       }
+          if (outContains(arrayCheckout, "ADDRESS")){
+        address=data_row.street_clean;
+        data.push(address);
+       }
+      if (outContains(arrayCheckout, "NATION")){
+
+        nation=data_row.NATIONALITY;
+        data.push(nation);
+       }
+     
+         if (outContains(arrayCheckout, "FNAME")){
+        name=data_row.NAME;
+        data.push(name);
+       }
+
+        
         var arr=[];
-        for (j=0;j<categ; j++){
+        for (j=0;j<outnumber; j++){
           arr[j]=document.createElement('td');
           arr[j].innerHTML=data[j];
           table_row.appendChild(arr[j]);
         }
         table.appendChild(table_row);
       }
+    }
     
     }
   
   table_populate();
-    var year=localStorage.getItem("year");
-    console.log(year);
-    //document.getElementById("oo").textContent= jsonOut;
+   localStorage.clear();
